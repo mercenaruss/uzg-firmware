694307d30bc477053732801d3e70f379eba5b2f4
xyzroe <me@kogan.in.ua>
2024-03-26 23:19:27 +0200

    v0.2.6
    Fix general settings save (Wi-Fi mode fix)

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.2.5"
+        #define VERSION "0.2.6"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-03-20 21:41:39.480174"
+        #define BUILD_TIMESTAMP "2024-03-26 23:18:21.389647"
     #endif
     
